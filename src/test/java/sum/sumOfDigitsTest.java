f6a84adc4c6eb5a57404b146f153b25258b2522a
johny <johny@JOHNY>
2025-09-16 15:30:47 +0530

    Updated

--- src/test/java/sum/sumOfDigitsTest.java
+++ src/test/java/sum/sumOfDigitsTest.java
@@ -1,5 +1,13 @@
 package sum;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class sumOfDigitsTest {
+	@Test
+	public void testcase1()
+	{
+		Assert.assertEquals(28,sumOfElements.sumOfDigits(7867));
+	}
 
 }
