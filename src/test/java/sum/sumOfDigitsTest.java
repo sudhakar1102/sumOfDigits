873f36cad570916eaa1ac11671bed07366bdcf49
HARSHA8799 <kasaganiharshavardhan7@gmail.com>
2025-09-16 15:21:27 +0530

    Completed

--- src/test/java/sum/sumOfDigitsTest.java
+++ src/test/java/sum/sumOfDigitsTest.java
@@ -1,5 +1,13 @@
 package sum;
 
+import org.junit.Test;
+
+import junit.framework.Assert;
+
 public class sumOfDigitsTest {
+	@Test
+	public void sum() {
+		Assert.assertEquals(24,sumOfElements.sumOfDigits(789));
+	}
 
 }
